96e71358cf66b3a07e7b5975de1d22e89faea0da
Tom Bigelajzen <tombigel@wix.com>
2025-04-21 09:07:46 +0300

    mock pointer events for tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -10,4 +10,25 @@ window.matchMedia = window.matchMedia || function() {
     addListener: function() {},
     removeListener: function() {}
   };
-}; 
+};
+
+// Mock PointerEvent methods for JSDOM
+if (typeof window !== 'undefined' && typeof window.HTMLElement !== 'undefined') {
+  window.HTMLElement.prototype.setPointerCapture = window.HTMLElement.prototype.setPointerCapture || function(pointerId) {};
+  window.HTMLElement.prototype.releasePointerCapture = window.HTMLElement.prototype.releasePointerCapture || function(pointerId) {};
+  // Mock hasPointerCapture to return true after setPointerCapture is called, needed for our component logic
+  const originalSetPointerCapture = window.HTMLElement.prototype.setPointerCapture;
+  const capturedPointers = new Set();
+  window.HTMLElement.prototype.setPointerCapture = function(pointerId) {
+    capturedPointers.add(pointerId);
+    originalSetPointerCapture.call(this, pointerId);
+  };
+  const originalReleasePointerCapture = window.HTMLElement.prototype.releasePointerCapture;
+  window.HTMLElement.prototype.releasePointerCapture = function(pointerId) {
+    capturedPointers.delete(pointerId);
+    originalReleasePointerCapture.call(this, pointerId);
+  };
+  window.HTMLElement.prototype.hasPointerCapture = window.HTMLElement.prototype.hasPointerCapture || function(pointerId) {
+    return capturedPointers.has(pointerId);
+  };
+} 
